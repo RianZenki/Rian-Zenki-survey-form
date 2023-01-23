ef6bc2f7deb4e7d60fdc5fee1b9dd3f54c12cf6d
Rian Zenki Nacazato <rian.zenki@gmail.com>
2023-01-23 05:01:54 -0300

    add responsiviness to the page and update validation script

--- dist/scripts/form-page1.js
+++ dist/scripts/form-page1.js
@@ -4,10 +4,15 @@ buttonForm.addEventListener("click", (event) => {
     event.preventDefault();
     const radioButton = document.querySelector('input[name="markets"]:checked');
     const select = document.querySelector("select");
-    if (radioButton && select.value) {
+    if (radioButton && select.value !== "default") {
+        const userInfos = {
+            market: radioButton.value,
+            accountStatus: select.value,
+        };
+        localStorage.setItem("userInfos", JSON.stringify(userInfos));
         location.href = "./form-page2.html";
     }
     else {
-        alert("Select one option to continue");
+        alert("Please, select an option to continue");
     }
 });
--- src/scripts/form-page1.ts
+++ src/scripts/form-page1.ts
@@ -3,12 +3,21 @@ const buttonForm = document.getElementById("next-button")!;
 buttonForm.addEventListener("click", (event: Event) => {
 	event.preventDefault();
 
-	const radioButton = document.querySelector('input[name="markets"]:checked');
+	const radioButton = document.querySelector(
+		'input[name="markets"]:checked'
+	) as HTMLInputElement | undefined;
 	const select = document.querySelector("select")!;
 
-	if (radioButton && select.value) {
+	if (radioButton && select.value !== "default") {
+		const userInfos = {
+			market: radioButton.value,
+			accountStatus: select.value,
+		};
+
+		localStorage.setItem("userInfos", JSON.stringify(userInfos));
+
 		location.href = "./form-page2.html";
 	} else {
-		alert("Select one option to continue");
+		alert("Please, select an option to continue");
 	}
 });
